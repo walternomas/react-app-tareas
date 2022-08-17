d17b7f08d8940cfe4768b7340ca986ffcaeaba1f
walternomas <w.barriosvazquez@gmail.com>
2022-08-17 04:07:07 -0300

    Mostrar la Lista de Tareas

--- src/componentes/ListaDeTareas.jsx
+++ src/componentes/ListaDeTareas.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
 import TareaFormulario from './TareaFormulario';
 import '../hojas-de-estilo/ListaDeTareas.css';
+import Tarea from './Tarea';
 
 function ListaDeTareas() {
 
-  const [tareas, setTareas] = useStates([]);
+  const [tareas, setTareas] = useState([{ texto:'nueva tarea', completada:true }]);
 
   return (
     <>
       <TareaFormulario />
       <div className='tareas-lista-contenedor'>
-        LISTA DE TAREAS
+        {
+          tareas.map((tarea) =>
+            <Tarea texto={tarea.texto} completada={tarea.completada} />
+          )
+        }
       </div>
     </>
   );
