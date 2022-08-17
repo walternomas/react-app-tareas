6e7f19382c7f56f2665ce95609be835445c630f2
walternomas <w.barriosvazquez@gmail.com>
2022-08-17 04:56:17 -0300

    Eliminar Tarea

--- src/componentes/ListaDeTareas.jsx
+++ src/componentes/ListaDeTareas.jsx
@@ -8,15 +8,16 @@ function ListaDeTareas() {
   const [tareas, setTareas] = useState([]);
 
   const agregarTarea = tarea => {
-    if(tarea.texto.trim()) {
+    if (tarea.texto.trim()) {
       tarea.texto = tarea.texto.trim();
       const tareasActualizadas = [tarea, ...tareas];
       setTareas(tareasActualizadas);
     }
   };
 
-  const eliminarTarea = tarea => {
-    setTareas(tareas.filter(t => t !== tarea));
+  const eliminarTarea = id => {
+    const tareasActualizadas = tareas.filter(tarea => tarea.id !== id);
+    setTareas(tareasActualizadas);
   };
 
   return (
@@ -25,11 +26,12 @@ function ListaDeTareas() {
       <div className='tareas-lista-contenedor'>
         {
           tareas.map((tarea) =>
-            <Tarea 
+            <Tarea
               key={tarea.id}
               id={tarea.id}
-              texto={tarea.texto} 
-              completada={tarea.completada} 
+              texto={tarea.texto}
+              completada={tarea.completada}
+              eliminarTarea={eliminarTarea}
             />
           )
         }
